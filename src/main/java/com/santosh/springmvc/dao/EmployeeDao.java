5b3acc9286ebcbdd203ad2f5c4a6cb6938d148f3
santy2509 <santosh.vy1144@gmail.com>
2016-05-13 18:21:31 -0700

    Registration using Spring-Hibernate and MYSQL

---
+++ src/main/java/com/santosh/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.santosh.springmvc.dao;
+
+import java.util.List;
+
+import com.santosh.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
---
+++ src/main/java/com/santosh/springmvc/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.santosh.springmvc.service;
+
+import java.util.List;
+
+import com.santosh.springmvc.model.Employee;
+
+public interface EmployeeService {
+
+	Employee findById(int id);
+	
+	void saveEmployee(Employee employee);
+	
+	void updateEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+
+	List<Employee> findAllEmployees(); 
+	
+	Employee findEmployeeBySsn(String ssn);
+
+	boolean isEmployeeSsnUnique(Integer id, String ssn);
+	
+}
